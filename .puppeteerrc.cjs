c20507eed08df83ae0cb2349cf064bfa7e0b3108
chandaillabs <saauryadityasinghchandail@gmail.com>
2026-08-25 02:49:59 +0530

    Update .puppeteerrc.cjs

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -4,6 +4,5 @@ const { join } = require('path');
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
-  // Changes the cache location for Puppeteer to Render's persistent cache
   cacheDirectory: join('/opt/render/.cache/puppeteer'),
 };
