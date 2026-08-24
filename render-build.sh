d17b9a812c5d33ba4289d379b450f998dac4f78f
chandaillabs <saauryadityasinghchandail@gmail.com>
2026-08-25 02:50:22 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
+# Exit on error
 set -o errexit
 
 npm install
+
+# Store/pull Puppeteer cache with build cache
+PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer
+mkdir -p $PUPPETEER_CACHE_DIR
+
+# Install Chrome for Puppeteer
 npx puppeteer browsers install chrome
